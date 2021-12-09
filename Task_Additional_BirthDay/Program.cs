d9e79444d2bef9764c402a7364b5d57cfbde1e6c
MSmaga <msmaga@protonmail.com>
2021-12-09 13:05:36 +0200

    Added namespace, Program, Main for VS2019

--- Task_Additional_BirthDay/Program.cs
+++ Task_Additional_BirthDay/Program.cs
@@ -1,22 +1,32 @@
-﻿#region Задание Дополнительное
-//Используя Visual Studio, создайте проект по шаблону Console Application. 
-//Реализуйте программу, которая будет принимать от пользователя дату его рождения и выводить 
-//количество дней до его следующего дня рождения. 
-#endregion
+﻿namespace Task_Additional_BirthDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание Дополнительное
+            //Используя Visual Studio, создайте проект по шаблону Console Application. 
+            //Реализуйте программу, которая будет принимать от пользователя дату его рождения и выводить 
+            //количество дней до его следующего дня рождения. 
+            #endregion
 
 
-// Input data
-Console.Write("Please, insert the day of your birth:");
-int dayBirth = Convert.ToInt32(Console.ReadLine());
+            // Input data
+            Console.Write("Please, insert the day of your birth:");
+            int dayBirth = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Please, insert the month of your birth:");
-int monthBirth = Convert.ToInt32(Console.ReadLine());
-
-
-// Read self-documented code! Ha -ha
-TimeSpan days = new DateTime(DateTime.Now.Year + 1, monthBirth, dayBirth) - DateTime.Now;
+            Console.Write("Please, insert the month of your birth:");
+            int monthBirth = Convert.ToInt32(Console.ReadLine());
 
 
-Console.Write($"Please, wait {days.Days} days!");
+            // Read self-documented code! Ha -ha
+            TimeSpan days = new DateTime(DateTime.Now.Year + 1, monthBirth, dayBirth) - DateTime.Now;
 
-Console.ReadLine();
+
+            Console.Write($"Please, wait {days.Days} days!");
+
+            Console.ReadLine();
+        }
+    }
+}
+
